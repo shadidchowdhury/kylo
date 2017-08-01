ff6acf84b655ed0012a9a8ab578087f6ed023138
Shadid Chowdhury <Shadid.Chowdhury@thinkbiganalytics.com>
2017-08-01 15:47:33 +0200

    Add data source on selection

--- ui/ui-app/src/main/resources/static/js/feed-mgr/data-explorer/dataExplorerController.js
+++ ui/ui-app/src/main/resources/static/js/feed-mgr/data-explorer/dataExplorerController.js
@@ -1,6 +1,6 @@
 define(['angular', "feed-mgr/data-explorer/module-name"], function (angular, moduleName) {
 
-    var controller = function ($scope, AlationDataExplorerService) {
+    var controller = function ($scope, $mdDialog, $mdToast, AlationDataExplorerService, DatasourcesService) {
         var self = this;
 
         AlationDataExplorerService.alationSdkInit('https://kylo.trialalation.com/');
@@ -12,6 +12,36 @@ define(['angular', "feed-mgr/data-explorer/module-name"], function (angular, mod
                 onSelect: function (data) {
                     console.log("data is selected");
                     console.log(data);
+
+                    var dataSourceModel = DatasourcesService.newJdbcDatasource();
+                    dataSourceModel.name = data.qualifiedName;
+                    dataSourceModel.description = data.qualifiedName;
+                    dataSourceModel.databaseConnectionUrl = data.dataSource.jdbcUri;
+                    dataSourceModel.databaseDriverClassName = "com.mysql.jdbc.Driver";
+                    dataSourceModel.databaseDriverLocation = "file:///opt/nifi/mysql/mysql-connector-java-5.0.8-bin.jar";
+                    dataSourceModel.databaseUser = "kylo";
+                    dataSourceModel.password = "test1234";
+
+
+                    DatasourcesService.save(dataSourceModel)
+                        .then(function (savedModel) {
+                            $mdToast.show(
+                                $mdToast.simple()
+                                    .textContent("Successfully added data source " + dataSourceModel.name + ".")
+                                    .hideDelay(3000)
+                            );
+                            return savedModel;
+                        }, function (err) {
+                            $mdDialog.show(
+                                $mdDialog.alert()
+                                    .clickOutsideToClose(true)
+                                    .title("Save Failed")
+                                    .textContent("The data source '" + model.name + "' could not be saved. " + err.data.message)
+                                    .ariaLabel("Failed to save data source")
+                                    .ok("Got it!")
+                            );
+                            return error;
+                        });
 
                     alationCatalogChooser.destroy();
                 },  // Callback for when user selects an object
@@ -38,6 +68,6 @@ define(['angular', "feed-mgr/data-explorer/module-name"], function (angular, mod
 
     };
 
-    angular.module(moduleName).controller('DataExplorerController', ["$scope", "AlationDataExplorerService", controller]);
+    angular.module(moduleName).controller('DataExplorerController', ["$scope", "$mdDialog", "$mdToast", "AlationDataExplorerService", "DatasourcesService", controller]);
 
 });
