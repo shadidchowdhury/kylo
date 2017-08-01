6b91fef95f05b851930ac9c3fce5b7cff7cf5525
Shadid Chowdhury <Shadid.Chowdhury@thinkbiganalytics.com>
2017-08-01 12:16:34 +0200

    Destroy catalog chooser upon selection

--- ui/ui-app/src/main/resources/static/js/feed-mgr/data-explorer/dataExplorerController.js
+++ ui/ui-app/src/main/resources/static/js/feed-mgr/data-explorer/dataExplorerController.js
@@ -6,23 +6,36 @@ define(['angular', "feed-mgr/data-explorer/module-name"], function (angular, mod
         AlationDataExplorerService.alationSdkInit('https://kylo.trialalation.com/');
 
         this.openCatalog = function () {
-            var alationCatalogChooser = AlationDataExplorerService.Alation.Catalog.createChooser({
+            var alationCatalogChooser = null;
+            alationCatalogChooser = AlationDataExplorerService.Alation.Catalog.createChooser({
                 embedMethod: AlationDataExplorerService.Alation.Catalog.ChooserEmbedMethod.MODAL,
                 onSelect: function (data) {
+                    console.log("data is selected");
+                    console.log(data);
+
+                    alationCatalogChooser.destroy();
                 },  // Callback for when user selects an object
                 onCancel: function () {
                 },  // Callback for when the user cancels
                 acceptObjectTypes: [
                     AlationDataExplorerService.Alation.Catalog.ObjectType.TABLE
-                ]  // List of acceptable oTypes (empty == all)
+                ],  // List of acceptable oTypes (empty == all)
+                acceptDataSourceTypes: [
+                    AlationDataExplorerService.Alation.DataSourceType.MYSQL
+                ]
             });
 
             alationCatalogChooser.open({
-                    acceptObjectTypes: [],
-                    acceptDataSourceTypes: []
+                acceptObjectTypes: [
+                    AlationDataExplorerService.Alation.Catalog.ObjectType.TABLE
+                ],  // List of acceptable oTypes (empty == all)
+                acceptDataSourceTypes: [
+                    AlationDataExplorerService.Alation.DataSourceType.MYSQL
+                ]
                 }
             );
-        }
+        };
+
     };
 
     angular.module(moduleName).controller('DataExplorerController', ["$scope", "AlationDataExplorerService", controller]);
